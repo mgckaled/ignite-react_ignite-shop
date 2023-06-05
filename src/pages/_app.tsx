e14f2d7c9ea1ac4b17ea8575c83f70cd62c771cf
mgckaled <marcelgckaled@gmail.com>
2023-06-05 20:43:32 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style
+        jsx
+        global
+      >{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
